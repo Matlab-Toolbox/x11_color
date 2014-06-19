04805a563170a6ce33d3f6c5fa8c025f3a5920dc
Morgan Prior <morgan@amaras-tech.co.uk>
2014-06-19 19:40:27 +0100

    Adding load_toolbox/package scripts

---
+++ load_package.m
@@ -0,0 +1,10 @@
+function load_package
+%% Script to load functions 
+
+%% Add all toplevel functions
+addpath([fileparts( mfilename('fullpath') ), '/package']);
+
+import x11_color.*
+import x11_rgb.*
+
+end
